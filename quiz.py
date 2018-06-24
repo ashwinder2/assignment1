539c82f5a11d1f02ba2c4a20641e856749f019d3
ashwinder2 <ashwinder1105@gmail.com>
2018-06-23 19:14:04 -0700

    add quiz

---
+++ quiz.py
@@ -0,0 +1,52 @@
+#anything print on a screen.
+print("welcome to our mathematical calculations programs")
+
+
+#1.write a python program for addition of two numbers.
+#for addition use '+'.
+x=int(input("enter the first no. "))
+y=int(input("enter the second no."))
+sum=x+y                             
+print("The sum is",sum)
+
+#2.Write a python program for multiplication of two numbers.
+#for multiplication use'*'operator.
+x=int(input("enter the first no. "))
+y=int(input("enter the second no."))
+multiply=x*y                         
+print("the multiplication of two numbers is",multiply)
+
+
+#3.Write a python program for division of two numbers.
+#for division use  '/' operator.
+x=int(input("enter the first no. "))
+y=int(input("enter the second no."))
+division=x/y                         
+print("the division of two numbers is",division)
+
+
+#4.Write a python program for find factorial of number.
+#for factorial use math module.
+import math
+print(math.factorial(int(input("enter any number: "))))
+
+
+#5.write a python program for find the value of raised to power.
+# for raised to power use pow module.
+import math
+print(math.pow(12,2))
+
+
+#6. Write a python program for subtraction.
+firstnum = float(input("enter first number. "))
+secondnum = float(input("enter second number. "))
+result = round(firstnum - secondnum, 2)
+print("the difference is ",result)
+
+#7.write a python program for floordivision.
+ #for floordivision use '//' operators.
+x=int(input("enter the first no. "))
+y=int(input("enter the second no."))
+result=x//y  
+print("the floordivision of two numbers is",result)
+
